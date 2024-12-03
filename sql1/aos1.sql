250e97e1af71542cebdff9bc0bc329eeccc8fbb1
Raghav Kuppan <raghav.kuppan@outlook.com>
2024-12-03 18:31:14 +0530

    Day 1

--- sql1/aos1.sql
+++ sql1/aos1.sql
@@ -1,12 +1,10 @@
 WITH intermediate as (select 
-	c.name as name, 
+	wl.child_id as child_id, 
 	wl.wishes->>'first_choice' as primary_wish,
 	wl.wishes->>'second_choice' as backup_wish,
 	wl.wishes -> 'colors' ->> 0 as favorite_color,
 	json_array_length(wl.wishes -> 'colors') as color_count
-from children c
-inner join wish_lists wl
-on c.child_id = wl.child_id),
+from wish_lists wl),
 complexity as (
 	select 
 		toy_id, 
@@ -24,12 +22,14 @@ complexity as (
 	from toy_catalogue
 )
 select 
-	id1.name, 
+	c.name, 
 	id1.primary_wish,
 	id1.backup_wish,
+	id1.favorite_color,
+	id1.color_count,
 	comp.gift_complexity,
 	comp.workshop_assignment
-from intermediate id1 
-inner join complexity comp
-on id1.primary_wish = comp.toy_name
-order by id1.name asc;
+from children c
+inner join intermediate id1 on c.child_id = id1.child_id
+inner join complexity comp on id1.primary_wish = comp.toy_name
+order by c.name asc;
